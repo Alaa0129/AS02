e167d71bb2a4766884f95fdc65753e429448edbd
Alaa0129 <alia@itu.dk>
2021-09-23 14:37:39 +0200

    Minor changes

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -17,9 +17,12 @@ namespace Assignment02
             };
             var s2 = new ImmutableStudent(1,"Mohammad","Hasham", Status.Active,new DateTime(2021, 09, 22, 00, 00, 00), new DateTime(2024, 06, 24, 00, 00, 00),new DateTime(2024, 06, 22, 00, 00, 00));
             var s3 = s2;
+            s3 = new ImmutableStudent(4,"Mohammad","Hasham", Status.Active,new DateTime(2021, 09, 22, 00, 00, 00), new DateTime(2024, 06, 24, 00, 00, 00),new DateTime(2024, 06, 22, 00, 00, 00));
+            var s4 = s2;
             s1.ToString();
             Console.WriteLine(s2);
-            Console.WriteLine(s2.Equals(s3));
+            Console.WriteLine(s3);
+            Console.WriteLine(s2.Equals(s4));
         }
     }
 }
